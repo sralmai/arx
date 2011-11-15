093bd3c6b4314f975cc23fed0569df7f5c73fd46
Jason Dusek <jason.dusek@gmail.com>
2011-11-15 16:45:10 +0000

    Benched Vector.write; allocates 77M of RAM.

--- Rebuild.hs
+++ Rebuild.hs
@@ -17,11 +17,24 @@ import Data.String
 import Data.Word
 
 import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as Vector
-import qualified Data.Vector.Unboxed.Mutable as Vector
+import qualified Data.Vector.Unboxed as Vector (create, length)
+import qualified Data.Vector.Unboxed.Mutable as Vector hiding (length)
 import qualified Data.Vector.Algorithms.Intro as Vector
 
 
+main                         =  do
+  bytes                     <-  Bytes.getContents
+--let out                    =  simple bytes
+--print (Bytes.length out)
+  let out                    =  rebuildAsVector bytes
+  print (Vector.length out)
+
+
+simple                      ::  ByteString -> ByteString
+simple                       =  Bytes.map (+1)
+
+
+rebuildAsVector             ::  ByteString -> Vector Word8
 rebuildAsVector bytes        =  byteVector
  where
   byteVector                ::  Vector Word8
@@ -29,10 +42,11 @@ rebuildAsVector bytes        =  byteVector
     counter                 <-  newSTRef 0
     v                       <-  Vector.new (Bytes.length bytes * 2)
     let write                =  writeOneByte v counter
-    for each b in bytes
-      write b
-    return (Vector.unsafeSlice 0 count v)
-  writeOneByte v counter b   =  do Vector.unsafeWrite v counter b
+    mapM_ write (Bytes.unpack bytes)
+    n                       <-  readSTRef counter
+    return (Vector.unsafeSlice 0 n v)
+  writeOneByte v counter b   =  do n <- readSTRef counter
+                                   Vector.unsafeWrite v n b
                                    modifySTRef counter (+1)
 
 
