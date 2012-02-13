550b385e8177c22f5d49dc0b8fa2d26affbb5927
Jason Dusek <jason.dusek@gmail.com>
2012-02-13 07:19:32 +0000

    First steps toward shell lib parser.

---
+++ System/Posix/ARX/ShLib/Parser.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings
+  #-}
+module System.Posix.ARX.ShLib.Parser where
+
+import Control.Applicative
+import Data.ByteString.Char8
+
+import Text.Regex.TDFA ( Regex, RegexContext(matchM),
+                         makeRegexOpts, defaultExecOpt,
+                         blankCompOpt,  lastStarGreedy )
+
+
+{-| Kinds of lines, from the point of view of the template parser. 
+ -}
+data Line                    =  FunctionOpen ByteString
+                             |  FunctionClose
+                             |  DefaultingVar ByteString
+                             |  Body
+deriving instance Show Line
+
+
+{-| Classify line as a template line or as a plain, code line. The classifier
+    marks function declarations and defaulting variables in a very simple
+    manner: the function open and close must be on a line by themselves, as
+    must all defaulting variable declarations that the classifier will
+    recognize.
+ -}
+classify                    ::  ByteString -> Line
+classify s =
+  maybe Body id (defaultingVar s <|> functionOpen s <|> functionClose s)
+
+
+defaultingVar s              =  do
+  (_, _, _, [name])         <-  pattern =~ s
+  Just (DefaultingVar name)
+ where
+  pattern = re "^ *: +[$][{]([_a-z0-9][_a-zA-Z0-9]*):=[^}]*[}]"
+
+functionOpen s               =  do
+  (_, _, _, [name])         <-  pattern =~ s
+  Just (FunctionOpen name)
+ where
+  pattern                    =  re "^([_a-z0-9][_a-zA-Z0-9]*)[(][)] +[{]"
+
+functionClose s              =  (pattern =~ s) >> Just FunctionClose
+ where
+  pattern                    =  re "^[}]"
+
+
+-- Regex stuff.
+
+(=~) :: Regex -> ByteString
+     -> Maybe (ByteString, ByteString, ByteString, [ByteString])
+(=~)                         =  matchM
+
+re                          ::  ByteString -> Regex
+re                           =  makeRegexOpts fullPosix defaultExecOpt
+ where
+  fullPosix                  =  blankCompOpt { lastStarGreedy=True }
+
