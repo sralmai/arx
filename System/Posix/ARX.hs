17a2823ae4fbed54a82b2d057798f8ed10d97ef5
Jason Dusek <jason.dusek@gmail.com>
2012-03-04 09:03:18 +0000

    Much cleaner, I think.

--- System/Posix/ARX.hs
+++ System/Posix/ARX.hs
@@ -4,26 +4,49 @@ module System.Posix.ARX where
 import Data.ByteString (ByteString)
 
 
-data ARX                     =  ARX ExecV Env Archives
-deriving instance Eq ARX
-deriving instance Ord ARX
-deriving instance Show ARX
+-- | A task to run on a @UNIX@ system. The task specification combines:
+-- * a program name and argument vector,
+-- * an environment mapping,
+-- * a directory to change to,
+-- * files to place in that directory.
+data Task                    =  Task ExecV Env Dir Files
+deriving (Eq, Ord, Show)
 
 
-data ByteStream              =  Literal ByteString | FromFile ByteString
-deriving instance Eq ByteSource
-deriving instance Ord ByteSource
-deriving instance Show ByteSource
+-- | A command and its arguments.
+data ExecV                   =  ExecV ByteString [ByteString]
+deriving (Eq, Ord, Show)
 
-data Archive                 =  Tar | TGZ | TBZ
-deriving instance Eq Archive
-deriving instance Ord Archive
-deriving instance Show Archive
 
-data Dir                     =  Fresh | Path ByteString
-deriving instance Eq Dir
-deriving instance Ord Dir
-deriving instance Show Dir
+-- | An environment mapping.
+type Env                     =  [(ByteString, ByteString)]
 
-type Archives = [(Dir, Archive, ByteStream)]
+
+-- | File archives or file retrieval functions -- using, for example, Git or
+--   HTTP -- and where to place their contents. Archives are unpacked in the
+--   order given.
+type Files                   =  [(Dir, FileSource)]
+
+data FileSource
+  -- | A file archive to be inlined in to the shell script, using the @SHDAT@
+  --   encoding.
+  = Archive ArchiveType ByteSource
+  -- | Used to call a shell library function for things like retrieving code
+  --   via Git or downloading and unpacking a tarball from S3.
+  | LibCall ExecV
+deriving (Eq, Ord, Show)
+
+-- | Things that can be read for bytes -- either a literal 'ByteString' or a
+--   path to a file.
+data ByteSource              =  Bytes ByteString | Path ByteString
+deriving (Eq, Ord, Show)
+
+-- | Types of supported archive.
+data ArchiveType             =  Tar | TBZ | TGZ
+deriving (Eq, Ord, Show)
+
+
+type Dir                     =  ByteString
+
+
 
