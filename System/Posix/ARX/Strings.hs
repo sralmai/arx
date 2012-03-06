b1e907aad4277bf9ede9c51cfade6208e564304f
Jason Dusek <jason.dusek@gmail.com>
2012-03-06 06:51:40 +0000

    Export a less evil function.

--- System/Posix/ARX/Strings.hs
+++ System/Posix/ARX/Strings.hs
@@ -13,13 +13,13 @@ import Data.String
 -- | This type represents the value and not the layout of a C string: a
 --   sequence of bytes not containing null.
 newtype CString = CString ByteString deriving (Eq, Ord, Show, Monoid)
-instance IsString CString  where fromString = fromString'
+instance IsString CString  where fromString = fromJust . maybeFromString
 instance Bytes CString     where bytes (CString s) = s
 instance Norm CString      where norm = CString . takeWhile (/= '\0')
 
 -- | A C string containing an @=@, separating the variable and the value.
 newtype Env = Env ByteString deriving (Eq, Ord, Show)
-instance IsString Env      where fromString = fromString'
+instance IsString Env      where fromString = fromJust . maybeFromString
 instance Bytes Env         where bytes (Env b) = b
 instance Norm Env          where norm b | elem '=' c = Env c
                                         | otherwise  = Env (snoc c '=')
@@ -27,7 +27,7 @@ instance Norm Env          where norm b | elem '=' c = Env c
 
 -- | A filename is a C string not containing @\/@.
 newtype Filename = Filename ByteString deriving (Eq, Ord, Show, Monoid)
-instance IsString Filename where fromString = fromString'
+instance IsString Filename where fromString = fromJust . maybeFromString
 instance Bytes Filename    where bytes (Filename s) = s
 instance Norm Filename     where norm = Filename . takeWhile condition
                                   where condition = not . (`elem` "/\0")
@@ -40,6 +40,6 @@ maybeNorm                   ::  (Bytes t, Norm t) => ByteString -> Maybe t
 maybeNorm b                  =  guard (bytes normed == b) >> Just normed
  where normed                =  norm b
 
-fromString'                 ::  (Bytes t, Norm t) => String -> t
-fromString'                  =  fromJust . maybeNorm . fromString
+maybeFromString             ::  (Bytes t, Norm t) => String -> Maybe t
+maybeFromString              =  maybeNorm . fromString
 
