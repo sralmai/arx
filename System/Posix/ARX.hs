242c9f51e7b84beff3048e09fe5e65b61830563b
Jason Dusek <jason.dusek@gmail.com>
2012-03-06 19:56:51 +0000

    Tentatively use composer.

--- System/Posix/ARX.hs
+++ System/Posix/ARX.hs
@@ -3,50 +3,41 @@ module System.Posix.ARX where
 
 import Data.ByteString (ByteString)
 
+import System.Posix.ARX.Composer
+
 
 -- | A task to run on a @UNIX@ system. The task specification combines:
+--
 -- * a program name and argument vector,
+--
 -- * an environment mapping,
+--
 -- * a directory to change to,
+--
 -- * files to place in that directory.
-data Task                    =  Task ExecV Env Dir Files
-deriving (Eq, Ord, Show)
-
-
--- | A command and its arguments.
-data ExecV                   =  ExecV ByteString [ByteString]
-deriving (Eq, Ord, Show)
-
-
--- | An environment mapping.
-type Env                     =  [(ByteString, ByteString)]
-
-
--- | File archives or file retrieval functions -- using, for example, Git or
---   HTTP -- and where to place their contents. Archives are unpacked in the
---   order given.
-type Files                   =  [(Dir, FileSource)]
+data Task
+  = Task { cmd :: (ByteString, [ByteString]) -- ^ Command and its arguments.
+         , env :: [(ByteString, ByteString)] -- ^ Environment mapping.
+         , dir :: ByteString -- ^ Desired working directory.
+         , files :: [(ByteString, FileSource)] -- ^ File sources (git, tars).
+         }
+ deriving (Eq, Ord, Show)
 
 data FileSource
   -- | A file archive to be inlined in to the shell script, using the @SHDAT@
   --   encoding.
   = Archive ArchiveType ByteSource
   -- | Used to call a shell library function for things like retrieving code
-  --   via Git or downloading and unpacking a tarball from S3.
+  --   via Git or downloading and unpacking a tarball over HTTP.
   | LibCall ExecV
-deriving (Eq, Ord, Show)
+ deriving (Eq, Ord, Show)
 
 -- | Things that can be read for bytes -- either a literal 'ByteString' or a
 --   path to a file.
 data ByteSource              =  Bytes ByteString | Path ByteString
-deriving (Eq, Ord, Show)
+ deriving (Eq, Ord, Show)
 
 -- | Types of supported archive.
 data ArchiveType             =  Tar | TBZ | TGZ
-deriving (Eq, Ord, Show)
-
-
-type Dir                     =  ByteString
-
-
+ deriving (Eq, Ord, Show)
 
