8ebbad26ba09ecf3695e3892a7bfaf6039946e55
Jason Dusek <jason.dusek@gmail.com>
2012-03-06 07:03:01 +0000

    No semantic changes; just a reformat. Making more flexible use of the layout rule and compacting the code.

--- System/Posix/ARX/Sh.hs
+++ System/Posix/ARX/Sh.hs
@@ -16,53 +16,40 @@ import qualified Text.ShellEscape as Esc
 import System.Posix.ARX.Strings
 
 
-setEU                       ::  ByteString
-setEU                        =  "set -e -u\n"
+setEU :: ByteString
+setEU  = "set -e -u\n"
 
 -- | Valid shell string values contain any byte but null.
-newtype Val                  =  Val CString
- deriving (Eq, Ord, Show, Monoid)
-instance Norm Val where
-  norm                       =  Val . norm
-instance IsString Val where
-  fromString                 =  Val . fromString
-instance Bytes Val where
-  bytes (Val c)              =  bytes c
-instance Sh Val where
-  sh                         =  Esc.bytes . Esc.sh . bytes
+newtype Val = Val CString deriving (Eq, Ord, Show, Monoid)
+instance IsString Val where fromString = Val . fromString
+instance Norm Val     where norm = Val . norm
+instance Bytes Val    where bytes (Val c) = bytes c
+instance Sh Val       where sh = Esc.bytes . Esc.sh . bytes
 
 
 -- | Valid shell variable names consist of a leading letter or underscore and
 --   then any number of letters, underscores or digits.
-newtype Var                  =  Var ByteString
- deriving (Eq, Ord, Show)
-instance Sh Var where
-  sh (Var bytes)             =  bytes
+newtype Var = Var ByteString deriving (Eq, Ord, Show)
+instance Sh Var where sh (Var bytes) = bytes
 
-var                         ::  ByteString -> Maybe Var
-var ""                       =  Nothing
-var bytes = guard (leading h && Bytes.all body t) >> Just (Var bytes)
- where
-  (h, t)                     =  (Bytes.head bytes, Bytes.tail bytes)
-  body c                     =  leading c || (c >= '0' && c <= '9')
-  leading c = (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_'
+var             :: ByteString -> Maybe Var
+var ""           = Nothing
+var bytes        = guard (leading h && Bytes.all body t) >> Just (Var bytes)
+ where (h, t)    = (Bytes.head bytes, Bytes.tail bytes)
+       body c    = leading c || (c >= '0' && c <= '9')
+       leading c = (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_'
 
 
 -- | Shell strings with variable substitution.
-newtype VarVal               =  VarVal [Either Var Val]
- deriving (Eq, Ord, Show)
-instance Sh VarVal where
-  sh (VarVal l)              =  (mconcat . map sh) l
-instance IsString VarVal where
-  fromString                 =  VarVal . (:[]) . Right . fromString
+newtype VarVal = VarVal [Either Var Val] deriving (Eq, Ord, Show)
+instance Sh VarVal       where sh (VarVal l) = (mconcat . map sh) l
+instance IsString VarVal where fromString = VarVal . (:[]) . Right . fromString
 
 
-instance Sh (Either Var Val) where
-  sh (Left var)              =  mconcat ["\"$", sh var, "\""]
-  sh (Right val)             =  sh val
+instance Sh (Either Var Val) where sh (Left var)  = mconcat ["\"$",sh var,"\""]
+                                   sh (Right val) = sh val
 
-instance Sh [Val] where
-  sh                         =  mconcat . map (mappend " " . sh)
+instance Sh [Val] where sh = mconcat . map (mappend " " . sh)
 
-class Sh t where sh         ::  t -> ByteString
+class Sh t where sh :: t -> ByteString
 
