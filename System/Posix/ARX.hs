b0ccdd348b4675681a389a61f833c1310305246d
Jason Dusek <jason.dusek@gmail.com>
2012-03-11 18:16:04 +0000

    Move fields around.

--- System/Posix/ARX.hs
+++ System/Posix/ARX.hs
@@ -65,16 +65,16 @@ data Executor = Executor
                    --   letter-digit-hypen rule common to DNS.
   , tmp :: Path -- ^ The directory where temporary directories are allocated.
                 --   The default is @/tmp@.
+  , dir :: Maybe Path -- ^ Directory to run task in, if a change is desired.
   , redirect :: Maybe Redirect -- ^ Redirection of @STDERR@ and @STDOUT@. The
                                --   default is not to redirect.
   , detach :: Maybe Detach -- ^ Make it possible for the process to run with
                            --   the terminal detached (for example, with
                            --   screen). The default is not to detach.
-  , dir :: Maybe Path -- ^ Directory to run task in, if a change is desired.
   }
 -- | Executor with defaults set.
-executor = Executor { tag="arx", tmp="/tmp", redirect=Nothing, detach=Nothing
-                    , dir=Nothing }
+executor = Executor { tag="arx", tmp="/tmp", dir=Nothing
+                    , redirect=Nothing, detach=Nothing }
 
 data Detach = Screen  -- TODO: add  | TMUX | NoHUP
 -- TODO: data LXC = LXC ...
