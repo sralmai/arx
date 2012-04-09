26fd4940f74c141505e4ee8604800d1ad30c0c8f
Jason Dusek <jason.dusek@gmail.com>
2012-04-09 08:05:52 +0000

    Converging lib and compiler.

--- System/Posix/ARX/Executor.hs
+++ System/Posix/ARX/Executor.hs
@@ -11,7 +11,7 @@ import System.Posix.ARX.Strings
 --   script always creates a temporary directory to unpack its libraries; a
 --   task without a directory set will execute in this temporary directory.
 --   Everything that represents an extension to the basic model of a
---   reproducible process hierarchy with files and environment variables is
+--   reproducible process hierarchy with files and environment Outerbles is
 --   captured by the 'Executor' data structure.
 data Executor = Executor
   { tag :: LDHName -- ^ A short prefix used for screens, temporary directories
@@ -32,7 +32,7 @@ executorDefaults = Executor { tag="arx", tmp=tmpDefaults, dir=Nothing
 
 data Detach = Screen -- TODO: add  | TMUX | NoHUP
 instance Compiled Detach where
-  compile (Screen _) = [CMD lib "screen_"]
+  compile Screen = [CMD lib "screen_setup", CMD lib "screen_"]
 
 data Redirect = Logger (Maybe CString)
 instance Compiled Redirect where
@@ -55,26 +55,39 @@ tmpDefaults = TMP "/tmp" True True
 --  * Compile the executor without tmpx and if we have to call back in to the
 --    library, add tmpx statements and recompile.
 
--- | Naively translate an executor to tokens, not handling needed/wanted
---   temporary directores.
+-- | Translate an Executor to tokens in preparation to joining it with env (if
+--   used) and...
 direct :: Executor -> [TOK]
-direct Executor{..} = case dir in
+direct Executor{..} = case detach,  in
+  Just Screen -> [ libInner "screen_setup",
+                   libInner "early_files",
   Nothing -> mconcat [ [CMD lib "tmpx"],
                        maybe [] ((++[CMD lib "trap_dot"]) . compile) detach,
                        maybe [] compile redirect ]
-data Executor = Executor
-  { tag :: LDHName -- ^ A short prefix used for screens, temporary directories
-                   --   and other resources allocated by @ARX@. The default
-                   --   is @arx@. The names are constrained by the
-                   --   letter-digit-hypen rule common to DNS.
-  , tmp :: TMP -- ^ Temporary directory creation and removal settings.
-  , dir :: Maybe Path -- ^ Directory to run task in, if a change is desired.
-  , redirect :: Maybe Redirect -- ^ Redirection of @STDERR@ and @STDOUT@. The
-                               --   default is not to redirect.
-  , detach :: Maybe Detach -- ^ Make it possible for the process to run with
-                           --   the terminal detached (for example, with
-                           --   screen). The default is not to detach.
-  }
+ where
+  screen_and_logger_with_tmp = do
+    Screen   <- detach
+    Logger _ <- redirect
+    Just [ libInner "tmp",
+           libInner "trap_on", Sh.VarVal [Left "dir"],
+           libInner "screen_setup",
+           libInner "meta_archives",
+           libInner "popd_",
+           libInner "cd_p", Sh.VarVal [Left "work_dir"],
+           libInner "early_archives",
+           libInner "trap_off", -- Remove trap since we are about to exit.
+           libOuter "screen_run",
+           libInner "trap_on", Sh.VarVal [Left "dir"],
+           libInner "logger_",
+           libInner "late_archives",
+           -- env
+           -- exec
+
+  --tag :: LDHName
+ -- tmp :: TMP
+-- dir :: Maybe Path
+-- redirect :: Maybe Redirect
+ -- detach :: Maybe Detach
 
 tmpVars :: TMP -> [Sh.VarVal]
 tmpVars TMP{..} = Sh.VarVal . (:[]) . Right . val <$>
@@ -85,8 +98,11 @@ tmpVars TMP{..} = Sh.VarVal . (:[]) . Right . val <$>
 
 class Compile t where compile :: t -> [TOK]
 
-lib :: CMD
-lib  = Lib True libPath
+lib :: Bool -> Sh.VarVal -> TOK
+lib b s = CMD (Lib b libPath) s
+
+libInner = lib False
+libOuter = lib True
 
 -- | The library is stored at @$dir/lib@, with @$dir@ set by the executor
 --   dynamically.
