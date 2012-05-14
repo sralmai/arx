0d9d0e325a790ecbcac8102f89a76a4ed75c9eea
Jason Dusek <jason.dusek@gmail.com>
2012-05-14 02:41:34 +0000

    Update to support conditional wrapper, per example. The "condition" will just be a ByteString since I want tight control over the layout of the generated code.

--- System/Posix/ARX/Composer.hs
+++ System/Posix/ARX/Composer.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings
-           , TupleSections
-           , PatternGuards #-}
+           , PostfixOperators
+           , TupleSections #-}
 
 module System.Posix.ARX.Composer where
 
 import Control.Applicative
 import Control.Monad
-import Data.ByteString.Char8 (ByteString, intercalate)
+import Data.ByteString.Char8 (ByteString, unwords)
 import Data.Monoid
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -16,51 +16,55 @@ import System.Posix.ARX.Strings
 import qualified System.Posix.ARX.Sh as Sh
 
 
--- | A sequence of labelled wrappers.
-data Wrapped label = Wrapped ExecutionContext [(label, [Sh.VarVal])]
- deriving (Eq, Ord, Show)
-instance Monoid (Wrapped t) where
-  mempty = Wrapped (Inline False Set.empty) []
-  Wrapped ctx defs `mappend` Wrapped _    []            = Wrapped ctx defs
-  Wrapped ctx defs `mappend` Wrapped ctx_ ((l, code):t) = Wrapped ctx' defs'
-   where (ctx', transit) = merge ctx ctx_
-         defs' = defs ++ (l, transit ++ code):t
-
 -- | Execution contexts for wrappers. For common wrappers, like @flock@ or
 --   @screen@, there's no real context needed; this is the @external@ context;
 --   but for shell functions that are used as wrappers we need a way to inline
 --   their code, escaped in a such a way that the calling shell (always @sh@
---   in ARX) simply passes it on.
+--   in ARX) simply passes it on. Most shell wrappers will call @exec@ to run
+--   the subordinate commands, so as not to give rise to massive process
+--   hierarchies.
 data ExecutionContext
   = InlineSh   Sh.VarVal -- ^ Sh code.
   | InlineBash Sh.VarVal -- ^ Bash code.
-  | External
+  | Plain
  deriving (Eq, Ord, Show)
 
--- | Merges two execution contexts and potentially inserts code to transition
---   between them. The merge of two 'Inline' contexts, for example, is an
---   inline context with definitions from both of them and the transition code
---   is empty; but the merge of most contexts is simply the leftmost one and
---   the code inserted by 'transition'.
-merge :: ExecutionContext -> ExecutionContext
-      -> (ExecutionContext, [Sh.VarVal])
-merge ctx' ctx = case ctx' of
-  Inline False c | Inline b c' <- ctx -> (Inline b (Set.union c c'), [ ]    )
-                 | otherwise          -> (ctx'                     , transit)
-  _                                   -> (ctx'                     , transit)
- where transit = transition ctx' ctx
+-- | Render an abstract command into an array of values suitable for use as a
+--   shell command line.
+class Compile t where compile :: t -> [ByteString]
 
--- | Insert code that transitions from the first context to the second.
-transition :: ExecutionContext -> ExecutionContext -> [Sh.VarVal]
-transition ctx' ctx = case ctx of
-  Inline _ codez | Inline False _ <- ctx' -> "exec" : inlined codez
-                 | otherwise              -> inlined codez
-  External       | Inline False _ <- ctx' -> ["exec"]
-                 | otherwise              -> []
- where inlined codez = ["/bin/sh","-c",Sh.VarVal [Right (inline codez)],"sh"]
+instance Compile (ExecutionContext, [Sh.VarVal]) where
+  compile (ctx, texts) = Sh.sh <$> case ctx of
+    Plain           -> texts
+    InlineSh   defs -> "sh"  :"-c":(defs +@):text:texts
+    InlineBash defs -> "bash":"-c":(defs +@):text:texts
+   where
+    (+@) (Sh.VarVal vs) = Sh.VarVal (vs ++ [(Right . Sh.Val) "\n\"$@\""])
+    text | text:_ <- texts = text -- Try to set $0 to something useful.
+         | otherwise       = "_"
 
-inline :: Set Sh.Val -> Sh.Val
-inline decls = norm code
- where
-  code = (intercalate "\n" . (++["\"$@\""])) (bytes <$> Set.toList decls)
+
+
+data Stacked = Simple (ExecutionContext, [Sh.VarVal])
+             | Conditional ByteString (ExecutionContext, [Sh.VarVal])
+ deriving (Eq, Ord, Show)
+
+instance Compile Stacked where
+  compile (Simple tuple) = "set" : "--" : (compile tuple ++ ["\"$@\""])
+  compile (Conditional expr tuple) = "!" : expr : "||" : compile (Simple tuple)
+
+
+{-
+  set -- sh -c "$work" sh "$@"
+  set -- env ... "$@"
+  ! $downloads || set -- sh -c '...' downloads ... "$@"
+  ! $dm_tmpx   || set -- sh -c '...' sh trap_on "$dir" "$@"
+  ! $dm        || set -- sh -c '...' screend ... "$@"
+  ! $dm_tmpx   || set -- trap_off "$@"
+  ! $sources   || set -- sources "$@"
+  ! $archives  || set -- archives "$@"
+  ! $tmpx      || set -- tmp ... "$@"
+  ## ! $dirs      || set -- mkdir_ ... "$@"
+  "$@"
+  -}
 
